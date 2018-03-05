7b1cce62ffd88c0a384fc3e75e8a7b549ade85d7
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-05 12:47:21 +0100

    Add abstarct BoardObject

---
+++ Shared/Board/BoardObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Board
+{
+    abstract class BoardObject
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
