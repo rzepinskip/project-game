fb09b16ce4246648456d824b44888d11dfe7a5f5
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-05 13:53:45 +0100

    Add basic GameMaster model
    pair programming: sowiks2711

---
+++ GameMaster/GameMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shared.Board;
+
+namespace GameMaster
+{
+    class GameMaster
+    {
+        Dictionary<string, Pawn> Players { get; }
+        Board Board { get; set; }
+    }
+}
