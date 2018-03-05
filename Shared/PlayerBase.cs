ede293e75810a39438086bc992697077ab3feda9
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-05 13:59:10 +0100

    Add base class of Player
    pair programming: sowiks2711

---
+++ Shared/PlayerBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Shared
+{
+    public class PlayerBase
+    {
+        public enum PlayerTeam {
+            [XmlEnum(Name = "red")]
+            Red,
+            [XmlEnum(Name = "blue")]
+            Blue
+        }
+
+        public enum TeamRole {
+            [XmlEnum(Name = "member")]
+            Member,
+            [XmlEnum(Name = "leader")]
+            Leader
+
+        }
+
+        private PlayerTeam Team { get; set; }
+        private TeamRole Type { get; set; }
+        private int Id { get; set; }
+    }
+}
