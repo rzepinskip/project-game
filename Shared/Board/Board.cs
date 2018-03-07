c14fc50e98d1cda91cc35c62eeeced9ccffe0d32
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-07 20:29:18 +0100

    Fix Board constructor
    Filling board content with Goal and Task Fields
    
    pair programming: sowiks

--- Shared/Board/Board.cs
+++ Shared/Board/Board.cs
@@ -15,6 +15,15 @@ namespace Shared.Board
             this.TaskAreaSize = taskAreaSize;
             this.Width = boardWidth;
             this.Content = new Field[boardWidth, taskAreaSize + 2 * goalAreaSize];
+
+            for (int i = 0; i < boardWidth; ++i) {
+                for (int j = 0; j < goalAreaSize; ++j)
+                    this.Content[i, j] = new GoalField();
+                for (int j = goalAreaSize; j < taskAreaSize + goalAreaSize; ++j)
+                    this.Content[i, j] = new TaskField();
+                for (int j = taskAreaSize + goalAreaSize; j < 2 * goalAreaSize + taskAreaSize; ++j)
+                    this.Content[i, j] = new GoalField();
+            }
         }
     }
 }
