669cd67ac18a1d959cb888e5919b0e9bb6d75eab
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-10 13:57:35 +0100

    Add MoveAvailabilityChain class
    Pair programming: sowiks

---
+++ GameMaster/ActionAvailability/MoveAvailabilityChain.cs
@@ -0,0 +1,35 @@
+﻿using Shared.Board;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Shared.CommonResources;
+using Shared;
+using GameMaster.ActionAvailability.AvailabilityChain;
+namespace GameMaster.ActionAvailability
+{
+    public class MoveAvailabilityChain
+    {
+
+        Location location;
+        MoveType direction;
+        Team team;
+        Board board;
+
+        public MoveAvailabilityChain(Location location, MoveType direction, Team team, Board board) 
+        {
+            this.location = location;
+            this.direction = direction;
+            this.team = team;
+            this.board = board;
+        }
+
+        public bool MoveAvailable() 
+        {
+            AvailabilityChainBuilder builder = new AvailabilityChainBuilder(new IsInsideBoardLink(location, direction, board.Width, board.Height))
+                .AddNextLink(new IsAvailableTeamAreaLink(location, direction, board.GoalAreaSize, board.TaskAreaSize, team))
+                .AddNextLink(new IsFieldPlayerUnoccupiedLink(location, direction, board));
+            return builder.Build().ValidateLink();
+        }
+        
+    }
+}
