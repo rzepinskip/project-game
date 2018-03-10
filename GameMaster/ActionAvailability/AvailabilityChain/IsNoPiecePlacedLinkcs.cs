5c86d9b1722517f3c012596d5fa7e1e9c02ad8a5
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-10 13:55:03 +0100

    Add new Link objects
    Pair programming: sowiks

---
+++ GameMaster/ActionAvailability/AvailabilityChain/IsNoPiecePlacedLinkcs.cs
@@ -0,0 +1,21 @@
+﻿using GameMaster.ActionAvailability.ActionAvailabilityHelpers;
+using Shared.Board;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.ActionAvailability.AvailabilityChain
+{
+    class IsNoPiecePlacedLinkcs : AvailabilityChainBase {
+        Location location;
+        Board board;
+        public IsNoPiecePlacedLinkcs(Location location, Board board) {
+            this.location = location;
+            this.board = board;
+        }
+
+        protected override bool Validate() {
+            return !PieceRelatedAvailability.IsPieceInCurrentLocation(location, board);
+        }
+    }
+}
---
+++ GameMaster/ActionAvailability/AvailabilityChain/IsPlayerCarryingPieceLink.cs
@@ -0,0 +1,20 @@
+﻿using GameMaster.ActionAvailability.ActionAvailabilityHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.ActionAvailability.AvailabilityChain
+{
+    class IsPlayerCarryingPieceLink : AvailabilityChainBase
+    {
+        public string playerGuid;
+        public Dictionary<string, int> playerGuidToPieceId;
+        public IsPlayerCarryingPieceLink(string playerGuid, Dictionary<string, int> playerGuidToPieceId) {
+            this.playerGuid = playerGuid;
+            this.playerGuidToPieceId = playerGuidToPieceId;
+        }
+        protected override bool Validate() {
+            return !PieceRelatedAvailability.HasPlayerEmptySlotForPiece(playerGuid, playerGuidToPieceId);
+        }
+    }
+}
