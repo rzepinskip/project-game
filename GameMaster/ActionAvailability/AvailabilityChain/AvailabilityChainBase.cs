63a8ba83b05b4228677b7b50d1890fea5de39a30
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-03-10 12:55:58 +0100

    Add chain of responsibility pattern base class for availability checks
    Pair programming: stelmaszekm

---
+++ GameMaster/ActionAvailability/AvailabilityChain/AvailabilityChainBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.ActionAvailability
+{
+    abstract class AvailabilityChainBase
+    {
+        protected AvailabilityChainBase nextLink;
+
+        abstract protected bool Validate();
+
+        public void SetNextLink(AvailabilityChainBase nextLink)
+        {
+            this.nextLink = nextLink;
+        }
+
+        public bool ValidateLink()
+        {
+            bool result = false;
+            if (this.Validate())
+                result = nextLink.ValidateLink();
+            return result;
+        }
+    }
+}
