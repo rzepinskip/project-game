b6e559df77b670368867dad1c882eb0a50812f54
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-03-10 10:05:49 +0100

    Add PickUpAvailability with unit tests
    Pair programming: stelmaszekm

---
+++ GameMaster.Tests/PickUpAvailabilityTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using GameMaster.ActionAvailability;
+
+namespace GameMaster.Tests
+{
+    public class PickUpAvailabilityTest
+    {
+
+        int boardWidth = 5;
+        int goalAreaSize = 2;
+        int taskAreaSize = 4;
+        string playerGuid = "c094cab7-da7b-457f-89e5-a5c51756035f";
+        Dictionary<string, int> playerGuidToPieceId;
+        Shared.Board.Location locationFail;
+        Shared.Board.Location locationSuccess;
+        Shared.Board.Location goalAreaLocation;
+
+        Shared.Board.Board board;
+
+        public PickUpAvailabilityTest() {
+            board = new Shared.Board.Board(boardWidth, taskAreaSize, goalAreaSize);
+            board.Content[1, 3].PlayerId = 1;
+            board.Content[3, 3].PlayerId = 2;
+            board.Content[2, 4].PlayerId = 3;
+            board.Content[2, 2].PlayerId = 4;
+            locationFail = new Shared.Board.Location() { X = 2, Y = 3 };
+            locationSuccess = new Shared.Board.Location() { X = 1, Y = 3 };
+            goalAreaLocation = new Shared.Board.Location() { X = 1, Y = 1 };
+
+            board.PlacePieceInTaskArea(1, locationSuccess);
+        }
+
+        [Fact]
+        public void PickUpOnEmptyTaskField()
+        {
+            Assert.False(PickUpAvailability.IsPieceInCurrentLocation(locationFail, board));
+        }
+        [Fact]
+        public void PickUpOnTaskFieldWithPiece()
+        {
+            Assert.True(PickUpAvailability.IsPieceInCurrentLocation(locationSuccess, board));
+        }
+        [Fact]
+        public void PickUpOnGoalArea()
+        {
+            Assert.False(PickUpAvailability.IsPieceInCurrentLocation(goalAreaLocation, board));
+        }
+        [Fact]
+        public void PickUpWhenPlayerNotCarringPiece()
+        {
+            Assert.True(PickUpAvailability.HasPlayerEmptySlotForPiece())
+        }
+    }
+}
---
+++ GameMaster/ActionAvailability/PickUpAvailability.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Board;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.ActionAvailability
+{
+    public static class PickUpAvailability
+    {
+        public static bool IsPieceInCurrentLocation(Location l, Board board)
+        {
+            int? PieceId = board.GetPieceFromBoard(l);
+            if (PieceId == null)
+                return false;
+            return true;
+        }
+        public static bool HasPlayerEmptySlotForPiece(string playerGuid, Dictionary<string, int> playerGuidToPiece)
+        {
+            return true;
+        }
+    }
+}
