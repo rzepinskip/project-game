7c0b1d3fdffa650e6da4bb3b60cc4aaf5f57f77d
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-03-10 10:03:38 +0100

    Add helper methods to board
    Pair programming: stelmaszekm

--- Shared/Board/Board.cs
+++ Shared/Board/Board.cs
@@ -25,5 +25,27 @@ namespace Shared.Board
                     this.Content[i, j] = new GoalField();
             }
         }
+        public bool IsLocationInTaskArea(Location l)
+        {
+            if (l.Y <= TaskAreaSize + GoalAreaSize - 1 && l.Y >= GoalAreaSize)
+                return true;
+            return false;
+
+        }
+        public void PlacePieceInTaskArea(int PieceId, Location l)
+        {
+            if (IsLocationInTaskArea(l))
+                ((TaskField)Content[l.X, l.Y]).PieceId = PieceId;
+        }
+        public int? GetPieceFromBoard(Location l)
+        {
+            int? pieceId = null;
+            if (IsLocationInTaskArea(l))
+            {
+                pieceId = ((TaskField)Content[l.X, l.Y]).PieceId;
+            }
+            return pieceId;
+
+        }
     }
 }
