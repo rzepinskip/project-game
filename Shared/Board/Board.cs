f3000e08bfd6a8adee4a612239becbe0dc8c60d4
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-10 13:50:18 +0100

    Add Height property to Board
    Pair programming: sowiks

--- Shared/Board/Board.cs
+++ Shared/Board/Board.cs
@@ -10,11 +10,17 @@ namespace Shared.Board
         public int TaskAreaSize { get; }
         public int GoalAreaSize { get; }
         public int Width { get; }
+        public int Height {
+            get
+            {
+                return 2 * this.GoalAreaSize + TaskAreaSize;
+            }
+        }
         public Board(int boardWidth, int taskAreaSize, int goalAreaSize) {
             this.GoalAreaSize = goalAreaSize;
             this.TaskAreaSize = taskAreaSize;
             this.Width = boardWidth;
-            this.Content = new Field[boardWidth, taskAreaSize + 2 * goalAreaSize];
+            this.Content = new Field[boardWidth, Height];
 
             for (int i = 0; i < boardWidth; ++i) {
                 for (int j = 0; j < goalAreaSize; ++j)
