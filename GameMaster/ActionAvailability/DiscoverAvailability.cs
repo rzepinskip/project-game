0474537b7f496b0e3f5f73dd68535b867b5347b5
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-03-10 10:05:18 +0100

    Add DiscoveryAvailability
    Pair programming: stelmaszekm

---
+++ GameMaster/ActionAvailability/DiscoverAvailability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.ActionAvailability
+{
+    class DiscoverAvailability
+    {
+        //always available
+        public static bool IsDiscoveryAvailable()
+        {
+            return true;
+        }
+
+    }
+}
