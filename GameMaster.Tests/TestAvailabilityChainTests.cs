079597a94d3514229eee1ea12744be759d3ad810
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-11 10:33:41 +0100

    Add TestAvailabilityChain and tests

---
+++ GameMaster.Tests/TestAvailabilityChainTests.cs
@@ -0,0 +1,40 @@
+﻿using GameMaster.ActionAvailability.AvailabilityChain;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GameMaster.Tests
+{
+    public class TestAvailabilityChainTests
+    {
+
+        string playerGuidSuccessTest = "c094cab7-da7b-457f-89e5-a5c51756035f";
+        string playerGuidFailTest = "c094cab7-da7b-457f-89e5-a5c51756035d";
+        Dictionary<string, int> playerGuidToPieceId;
+        int pieceId = 1;
+
+        Shared.Board.Board board;
+
+        public TestAvailabilityChainTests()
+        {
+
+            playerGuidToPieceId = new Dictionary<string, int>();
+            playerGuidToPieceId.Add(playerGuidSuccessTest, pieceId);
+        }
+
+        [Fact]
+        public void PlayerTestWhenCarryingPiece()
+        {
+            var testAvailabilityChain = new TestAvailabilityChain(playerGuidSuccessTest, playerGuidToPieceId);
+            Assert.True(testAvailabilityChain.ActionAvailable());
+        }
+
+        [Fact]
+        public void PlayerTestWhenCarryingNoPiece()
+        {
+            var testAvailabilityChain = new TestAvailabilityChain(playerGuidFailTest, playerGuidToPieceId);
+            Assert.False(testAvailabilityChain.ActionAvailable());
+        }
+
+
+    }
+}
---
+++ GameMaster/ActionAvailability/AvailabilityChain/TestAvailabilityChain.cs
@@ -0,0 +1,23 @@
+﻿using GameMaster.ActionAvailability.AvailabilityLink;
+using System.Collections.Generic;
+
+namespace GameMaster.ActionAvailability.AvailabilityChain
+{
+    public class TestAvailabilityChain : IAvailabilityChain
+    {
+        private string playerGuid;
+        private Dictionary<string, int> playerGuidToPiece;
+
+        public TestAvailabilityChain(string playerGuid, Dictionary<string, int> playerGuidToPiece)
+        {
+            this.playerGuid = playerGuid;
+            this.playerGuidToPiece = playerGuidToPiece;
+        }
+
+        public bool ActionAvailable()
+        {
+            var builder = new AvailabilityChainBuilder(new IsPlayerCarryingPieceLink(playerGuid, playerGuidToPiece));
+            return builder.Build().ValidateLink();
+        }
+    }
+}
