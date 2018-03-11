e31f1789d617d77648d6d31d0761b29237dacb6d
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-11 09:51:20 +0100

    Move builder and add interface for availabilitychains

---
+++ GameMaster/ActionAvailability/AvailabilityChain/AvailabilityChainBuilder.cs
@@ -0,0 +1,32 @@
+﻿using GameMaster.ActionAvailability.AvailabilityLink;
+
+
+namespace GameMaster.ActionAvailability
+{
+    class AvailabilityChainBuilder
+    {
+        AvailabilityLinkBase first;
+        AvailabilityLinkBase last;
+        private IsPieceInCurrentLocationLink isPieceInCurrentLocationLink;
+
+        public AvailabilityChainBuilder(AvailabilityLinkBase first) {
+            this.first = first;
+            this.last = first;
+        }
+
+        public AvailabilityChainBuilder(IsPieceInCurrentLocationLink isPieceInCurrentLocationLink)
+        {
+            this.isPieceInCurrentLocationLink = isPieceInCurrentLocationLink;
+        }
+
+        public AvailabilityChainBuilder AddNextLink(AvailabilityLinkBase nextLink) {
+            this.last.SetNextLink(nextLink);
+            this.last = nextLink;
+            return this;
+        }
+
+        public AvailabilityLinkBase Build() {
+            return first;
+        }
+    }
+}
---
+++ GameMaster/ActionAvailability/AvailabilityChain/IAvailabilityChain.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameMaster.ActionAvailability.AvailabilityChain
+{
+    interface IAvailabilityChain
+    {
+    }
+}
