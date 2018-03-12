32c47345098e82009fa2f81e2362139d21a27f77
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-03-12 15:30:31 +0100

    Add Test Piece method

--- Shared/GameMessages/TestPiece.cs
+++ Shared/GameMessages/TestPiece.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 using Shared.BoardObjects;
+using Shared.ResponseMessages;
 
 namespace Shared.GameMessages.PieceActions
 {
     [XmlRoot(Namespace = "https://se2.mini.pw.edu.pl/17-results/")]
     public class TestPiece : GameMessage
     {
-        public override void CanExecute(BoardObjects.Board board)
+        public override ResponseMessage Execute(Board board)
         {
-            throw new NotImplementedException();
-        }
+            var playerPiece = board.Players[PlayerId].Piece;
 
-        public override void Execute(BoardObjects.Board board)
-        {
-            throw new NotImplementedException();
+            var response = new TestPieceResponse()
+            {
+                PlayerId = this.PlayerId,
+                Piece = playerPiece
+            };
+
+            return response;
         }
     }
 }
