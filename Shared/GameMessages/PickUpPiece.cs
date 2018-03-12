1573f6a1eaf86fe46e6716feba05a0371e196862
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-03-12 15:30:08 +0100

    Add PickUp Piece method

--- Shared/GameMessages/PickUpPiece.cs
+++ Shared/GameMessages/PickUpPiece.cs
@@ -1,23 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
 using System.Xml.Serialization;
 using Shared.BoardObjects;
+using Shared.ResponseMessages;
 
 namespace Shared.GameMessages
 {
     [XmlRoot(Namespace = "https://se2.mini.pw.edu.pl/17-results/")]
     public class PickUpPiece : GameMessage
     {
-        public override void CanExecute(BoardObjects.Board board)
+        public override ResponseMessage Execute(Board board)
         {
-            throw new NotImplementedException();
-        }
+            var response = new PickUpPieceResponse();
 
-        public override void Execute(BoardObjects.Board board)
-        {
-            throw new NotImplementedException();
+            var player = board.Players[PlayerId];
+
+            if (!board.IsLocationInTaskArea(player.Location))
+                return response;
+
+            var playerField = board.Content[player.Location.X, player.Location.Y] as TaskField;
+
+            if (!playerField.PieceId.HasValue)
+                return response;
+
+            var piece = board.Pieces[playerField.PieceId.Value];
+            piece.PlayerId = PlayerId;
+
+            response = new PickUpPieceResponse()
+            {
+                PlayerId = this.PlayerId,
+                Piece = new Piece()
+                {
+                    Id = piece.Id,
+                    PlayerId = piece.PlayerId,
+                    Type = CommonResources.PieceType.Unknown
+                }
+            };
+
+            return response;
         }
     }
 
