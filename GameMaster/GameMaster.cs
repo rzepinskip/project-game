cae08aac16253d242339c36e8ddf5ef9d58c6f2e
Ryszard Szymański <ryszard.szymanski@outlook.com>
2018-03-14 21:43:25 +0100

    Add writing to CSV file

--- GameMaster/GameMaster.cs
+++ GameMaster/GameMaster.cs
@@ -5,32 +5,23 @@ using System.Text;
 using Shared;
 using Shared.BoardObjects;
 using Shared.GameMessages;
+using CsvHelper;
 
 namespace GameMaster
 {
-    class GameMaster
+    public class GameMaster
     {
         Dictionary<string, int> PlayerGuidToId { get; }
         Board Board { get; set; }
 
-        //private ActionLog GetLog(GameMessage m)
-        //{
-        //    var playerId = PlayerGuidToId[m.PlayerGuid];
-        //    var playerInfo = Board.Players[playerId];
-
-        //    return new ActionLog(playerId, playerInfo, m);
-        //}
-
-        private void PutLog(string filename, ActionLog log)
+        public void PutLog(string filename, ActionLog log)
         {
-            if (!File.Exists(filename))
-            {
-                string logsHeader = string.Join('\t', "Type", "Timestamp", "Game ID",
-                    "Player ID", "Player GUID", "Colour", "Role");
-                File.WriteAllText(filename, logsHeader);
-            }
-
-            File.WriteAllText(filename, log.ToString());
+            using (var textWriter = new StreamWriter(filename, true))
+                using (var csvWriter = new CsvWriter(textWriter))
+                {
+                csvWriter.NextRecord();
+                csvWriter.WriteRecord(log);
+                }
         }
     }
 }
