836591e019115315e1d7d1d1bb34e1d9d382f9c9
Unknown <wojtek.trze@gmail.com>
2018-03-18 00:01:17 +0100

    Add Move action tests

---
+++ Shared.Tests/ActionTests/ActionTest.cs
@@ -0,0 +1,91 @@
+﻿using Shared.BoardObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Tests.ActionTests
+{
+    public class ActionTest
+    {
+        protected int PlayerId = 1;
+
+        protected Board _gameMasterBoard;
+        protected Board _playerBoard;
+
+        public ActionTest()
+        {
+            _gameMasterBoard = GenerateGameMasterBoard();
+            _playerBoard = BoardGeneratePlayerBoard();
+        }
+
+        private Board GenerateGameMasterBoard()
+        {
+            var board = GenerateBoard();
+
+            var piece1 = new Piece
+            {
+                Id = 1,
+                Type = CommonResources.PieceType.Sham
+            };
+
+            var piece2 = new Piece
+            {
+                Id = 2,
+                Type = CommonResources.PieceType.Normal
+            };
+
+            board.PlacePieceInTaskArea(piece1.Id, new Location(0, 6));
+            board.PlacePieceInTaskArea(piece2.Id, new Location(2,2));
+            board.Pieces.Add(piece1.Id, piece1);
+            board.Pieces.Add(piece2.Id, piece2);
+
+            var testedPlayerInfo = new PlayerInfo
+            {
+                Team = CommonResources.TeamColour.Red
+            };
+            board.Players.Add(PlayerId, testedPlayerInfo);
+
+            var playerId = 2;
+            var playerInfo = new PlayerInfo
+            {
+                Team = CommonResources.TeamColour.Blue,
+                Location = new Location(2,4),
+                
+            };
+            board.Players.Add(playerId, playerInfo);
+            board.Content[playerInfo.Location.X, playerInfo.Location.Y].PlayerId = playerId;
+
+
+            return board;
+        }
+        private Board BoardGeneratePlayerBoard()
+        {
+            var board = GenerateBoard();
+
+            var playerInfo = new PlayerInfo
+            {
+                Team = CommonResources.TeamColour.Red
+            };
+
+            board.Players.Add(PlayerId, playerInfo);
+
+
+            return board;
+        }
+        private Board GenerateBoard()
+        {
+            var board = new Board(4, 4, 2);
+
+            return board;
+        }
+
+        protected void SetTestedPlayerLocation(Location location)
+        {
+            _gameMasterBoard.Content[location.X, location.Y].PlayerId = PlayerId;
+            _playerBoard.Content[location.X, location.Y].PlayerId = PlayerId;
+
+            _gameMasterBoard.Players[PlayerId].Location = location;
+            _playerBoard.Players[PlayerId].Location = location;
+        }
+    }
+}
---
+++ Shared.Tests/ActionTests/MoveTest.cs
@@ -0,0 +1,126 @@
+﻿using Shared.BoardObjects;
+using Shared.GameMessages;
+using Shared.ResponseMessages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Shared.Tests.ActionTests
+{
+    public class MoveTest : ActionTest
+    {
+        private Move GetMoveMessage(CommonResources.MoveType direction)
+        {
+            return new Move()
+            {
+                PlayerId = this.PlayerId,
+                Direction = direction
+            };
+
+        }
+
+        [Fact]
+        public void BasicMove()
+        {
+            SetTestedPlayerLocation(new Location(1,3));
+
+            var message = GetMoveMessage(CommonResources.MoveType.Right);
+            if (message.Execute(_gameMasterBoard) is MoveResponse response)
+                response.Update(_playerBoard);
+
+            Assert.True(AssertPlayerLocation(new Location(2,3)));
+        }
+
+        [Fact]
+        public void MoveOnPiece()
+        {
+            SetTestedPlayerLocation(new Location(1,2));
+
+            var message = GetMoveMessage(CommonResources.MoveType.Right);
+            if (message.Execute(_gameMasterBoard) is MoveResponse response)
+                response.Update(_playerBoard);
+
+            Assert.True(AssertPlayerLocation(new Location(2,2)));
+            Assert.True(AssertPiece(new Location(2,2), 2));
+        }
+
+
+        [Fact]
+        public void MoveOutsideBoard()
+        {
+            SetTestedPlayerLocation(new Location(0,3));
+
+            var message = GetMoveMessage(CommonResources.MoveType.Left);
+            if (message.Execute(_gameMasterBoard) is MoveResponse response)
+                response.Update(_playerBoard);
+
+            Assert.True(AssertPlayerLocation(new Location(0,3)));
+        }
+
+        [Fact]
+        public void MoveOnEnemyGoalArea()
+        {
+            SetTestedPlayerLocation(new Location(1,2));
+
+            var message = GetMoveMessage(CommonResources.MoveType.Down);
+            if (message.Execute(_gameMasterBoard) is MoveResponse response)
+                response.Update(_playerBoard);
+
+            Assert.True(AssertPlayerLocation(new Location(1,2)));
+        }
+
+        [Fact]
+        public void MoveOnPlayer()
+        {
+            SetTestedPlayerLocation(new Location(1,4));
+
+            var message = GetMoveMessage(CommonResources.MoveType.Right);
+            if (message.Execute(_gameMasterBoard) is MoveResponse response)
+                response.Update(_playerBoard);
+
+            Assert.True(AssertPlayerLocation(new Location(1,4)));
+        }
+
+        private bool AssertPlayerLocation(Location location)
+        {
+            if (location == null)
+                return false;
+
+            return AssertPlayerLocationOnBoard(location, _gameMasterBoard) && AssertPlayerLocationOnBoard(location, _playerBoard);
+
+        }
+        private bool AssertPlayerLocationOnBoard(Location location, Board board)
+        {
+            if (board.Content[location.X, location.Y].PlayerId != PlayerId)
+                return false;
+
+            if (board.Players[PlayerId].Location != location)
+                return false;
+
+            foreach (var field in board.Content)
+            {
+                if ((Location)field != location && field.PlayerId == PlayerId)
+                    return false;
+            }
+
+            return true;
+        }
+
+        private bool AssertPiece(Location location, int pieceId)
+        {
+            if (location == null)
+                return false;
+
+            var piece = _playerBoard.Pieces[pieceId];
+            if (piece == null || piece.Id != pieceId)
+                return false;
+
+            if (_playerBoard.Content[location.X, location.Y] is TaskField taskField)
+                if (taskField.PieceId == pieceId)
+                    return true;
+
+            return false;
+        }
+    }
+}
