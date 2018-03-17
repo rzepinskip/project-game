b2ad1b8e7113e2c10ae21dcd3f3343dfdc477363
Unknown <wojtek.trze@gmail.com>
2018-03-18 00:13:11 +0100

    Improve move action tests

--- Shared.Tests/ActionTests/MoveTest.cs
+++ Shared.Tests/ActionTests/MoveTest.cs
@@ -23,104 +23,90 @@ namespace Shared.Tests.ActionTests
         [Fact]
         public void BasicMove()
         {
-            SetTestedPlayerLocation(new Location(1,3));
+            SetTestedPlayerLocation(new Location(1, 3));
 
             var message = GetMoveMessage(CommonResources.MoveType.Right);
             if (message.Execute(_gameMasterBoard) is MoveResponse response)
                 response.Update(_playerBoard);
 
-            Assert.True(AssertPlayerLocation(new Location(2,3)));
+            AssertPlayerLocation(new Location(2, 3));
         }
 
         [Fact]
         public void MoveOnPiece()
         {
-            SetTestedPlayerLocation(new Location(1,2));
+            SetTestedPlayerLocation(new Location(1, 2));
 
             var message = GetMoveMessage(CommonResources.MoveType.Right);
             if (message.Execute(_gameMasterBoard) is MoveResponse response)
                 response.Update(_playerBoard);
 
-            Assert.True(AssertPlayerLocation(new Location(2,2)));
-            Assert.True(AssertPiece(new Location(2,2), 2));
+            AssertPlayerLocation(new Location(2, 2));
+            AssertPiece(new Location(2, 2), 2);
         }
 
 
         [Fact]
         public void MoveOutsideBoard()
         {
-            SetTestedPlayerLocation(new Location(0,3));
+            SetTestedPlayerLocation(new Location(0, 3));
 
             var message = GetMoveMessage(CommonResources.MoveType.Left);
             if (message.Execute(_gameMasterBoard) is MoveResponse response)
                 response.Update(_playerBoard);
 
-            Assert.True(AssertPlayerLocation(new Location(0,3)));
+            AssertPlayerLocation(new Location(0, 3));
         }
 
         [Fact]
         public void MoveOnEnemyGoalArea()
         {
-            SetTestedPlayerLocation(new Location(1,2));
+            SetTestedPlayerLocation(new Location(1, 2));
 
             var message = GetMoveMessage(CommonResources.MoveType.Down);
             if (message.Execute(_gameMasterBoard) is MoveResponse response)
                 response.Update(_playerBoard);
 
-            Assert.True(AssertPlayerLocation(new Location(1,2)));
+            AssertPlayerLocation(new Location(1, 2));
         }
 
         [Fact]
         public void MoveOnPlayer()
         {
-            SetTestedPlayerLocation(new Location(1,4));
+            SetTestedPlayerLocation(new Location(1, 4));
 
             var message = GetMoveMessage(CommonResources.MoveType.Right);
             if (message.Execute(_gameMasterBoard) is MoveResponse response)
                 response.Update(_playerBoard);
 
-            Assert.True(AssertPlayerLocation(new Location(1,4)));
+            AssertPlayerLocation(new Location(1, 4));
         }
 
-        private bool AssertPlayerLocation(Location location)
+        private void AssertPlayerLocation(Location location)
         {
-            if (location == null)
-                return false;
-
-            return AssertPlayerLocationOnBoard(location, _gameMasterBoard) && AssertPlayerLocationOnBoard(location, _playerBoard);
-
+            AssertPlayerLocationOnBoard(location, _gameMasterBoard);
+            AssertPlayerLocationOnBoard(location, _playerBoard);
         }
-        private bool AssertPlayerLocationOnBoard(Location location, Board board)
+        private void AssertPlayerLocationOnBoard(Location location, Board board)
         {
-            if (board.Content[location.X, location.Y].PlayerId != PlayerId)
-                return false;
+            Assert.Equal(PlayerId, board.Content[location.X, location.Y].PlayerId);
 
-            if (board.Players[PlayerId].Location != location)
-                return false;
+            Assert.Equal(board.Players[PlayerId].Location, location);
 
             foreach (var field in board.Content)
             {
-                if ((Location)field != location && field.PlayerId == PlayerId)
-                    return false;
+                if ((Location)field != location)
+                    Assert.NotEqual(PlayerId, field.PlayerId);
             }
-
-            return true;
         }
 
-        private bool AssertPiece(Location location, int pieceId)
+        private void AssertPiece(Location location, int pieceId)
         {
-            if (location == null)
-                return false;
-
             var piece = _playerBoard.Pieces[pieceId];
-            if (piece == null || piece.Id != pieceId)
-                return false;
+            Assert.Equal(pieceId, piece.Id);
 
-            if (_playerBoard.Content[location.X, location.Y] is TaskField taskField)
-                if (taskField.PieceId == pieceId)
-                    return true;
-
-            return false;
+            var taskField = _playerBoard.Content[location.X, location.Y] as TaskField;
+            Assert.Equal(pieceId, taskField.PieceId);
         }
     }
 }
