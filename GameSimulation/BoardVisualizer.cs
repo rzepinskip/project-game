f17f5b04634d376038dc37c9e7c7c90d46be3a30
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-03-18 19:46:45 +0100

    Fix visualization flickering

--- GameSimulation/BoardVisualizer.cs
+++ GameSimulation/BoardVisualizer.cs
@@ -8,46 +8,50 @@ namespace GameSimulation
     {
         public void VisualizeBoard(Board board)
         {
-            Console.Clear();
+            var output = new StringBuilder();
             for (int i = board.Height - 1; i >= 0; i--)
             {
                 for (int j = 0; j < board.Width; j++)
                 {
-                    var output = new StringBuilder();
+                    var objectSymbol = new StringBuilder();
 
                     var field = board.Content[j, i];
                     if (field.PlayerId != null)
                     {
                         var player = board.Players[field.PlayerId.Value];
                         if (player.Team == Shared.CommonResources.TeamColour.Red)
-                            output.Append("R");
+                            objectSymbol.Append("R");
                         else
-                            output.Append("B");
+                            objectSymbol.Append("B");
 
                         if (player.Piece != null)
-                            output.Append("p");
+                            objectSymbol.Append("p");
                         else
-                            output.Append(" ");
+                            objectSymbol.Append(" ");
                     }
                     else if (field is GoalField goalField)
                     {
                         if (goalField.Type == Shared.CommonResources.GoalFieldType.Goal)
-                            output.Append("G ");
+                            objectSymbol.Append("G ");
                         else
-                            output.Append("+ ");
+                            objectSymbol.Append("+ ");
                     }
                     else if (field is TaskField taskField)
                     {
                         if (taskField.PieceId != null)
-                            output.Append("p ");
+                            objectSymbol.Append("p ");
                         else
-                            output.Append("- ");
+                            objectSymbol.Append("- ");
                     }
-                    Console.Write(output.ToString());
+                    output.Append(objectSymbol);
                 }
-                Console.WriteLine();
+                output.AppendLine();
             }
 
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+
+            Console.WriteLine(output.ToString());
         }
     }
 }
