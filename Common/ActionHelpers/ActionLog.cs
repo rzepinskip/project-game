3bfc999e5e3ffe665ce800f287c1ce8237e0e27f
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-03-22 08:50:36 +0100

    Move action helpers to Common

---
+++ Common/ActionHelpers/ActionLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Common.ActionHelpers
+{
+    public class ActionLog
+    {
+        public ActionLog(int playerId, int gameId, string playerGuid, PlayerInfo playerInfo,
+            ActionType actionType)
+        {
+            ActionType = actionType;
+            Timestamp = DateTime.Now;
+            GameID = gameId;
+            PlayerID = playerId;
+            PlayerGUID = playerGuid;
+            Colour = playerInfo.Team;
+            Role = playerInfo.Role;
+        }
+
+        public ActionType ActionType { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int GameID { get; set; }
+        public int PlayerID { get; set; }
+        public string PlayerGUID { get; set; }
+        public TeamColor Colour { get; set; }
+        public PlayerType Role { get; set; }
+
+        public override string ToString()
+        {
+            return string.Join('\t', ActionType, Timestamp, GameID, PlayerID, PlayerGUID, Colour, Role);
+        }
+    }
+}
