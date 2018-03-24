b949389a41c2cb92b24c1f7aa6aa6643d9a05f1a
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-03-24 16:42:03 +0100

    Refactor Player project with Resharper once again

--- Player/Strategy/ListExtensions.cs
+++ Player/Strategy/ListExtensions.cs
@@ -4,16 +4,16 @@ using System.Threading;
 
 namespace Player.Strategy
 {
-    static class ListExtensions
+    internal static class ListExtensions
     {
         public static void Shuffle<T>(this IList<T> list)
         {
-            int n = list.Count;
+            var n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
-                T value = list[k];
+                var k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                var value = list[k];
                 list[k] = list[n];
                 list[n] = value;
             }
