71e2e8cf36606f0e1e4332e8e913af1688be314f
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-03-24 12:22:59 +0100

    Move game end condition implementation to GameMasterBoard

--- GameMaster/GameMasterBoard.cs
+++ GameMaster/GameMasterBoard.cs
@@ -1,12 +1,46 @@
-﻿using Common.BoardObjects;
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Common.BoardObjects;
 using Common.Interfaces;
 
 namespace GameMaster
 {
-    public class GameMasterBoard : BoardBase, IBoard
+    public class GameMasterBoard : BoardBase, IGameMasterBoard
     {
         public GameMasterBoard(int boardWidth, int taskAreaSize, int goalAreaSize) : base(boardWidth, taskAreaSize, goalAreaSize)
         {
+        }
+
+        public HashSet<Location> UncompletedBlueGoalsLocations { get; set; } = new HashSet<Location>();
+        public HashSet<Location> UncompletedRedGoalsLocations { get; set; } = new HashSet<Location>();
+
+        public void MarkGoalAsCompleted(GoalField goal)
+        {
+            switch (goal.Team)
+            {
+                case TeamColor.Blue:
+                    UncompletedBlueGoalsLocations.Remove(goal);
+                    break;
+                case TeamColor.Red:
+                    UncompletedRedGoalsLocations.Remove(goal);
+                    break;
+            }
+        }
+
+        public bool IsGameFinished()
+        {
+            return UncompletedBlueGoalsLocations.Count == 0 || UncompletedRedGoalsLocations.Count == 0;
+        }
+
+        public TeamColor CheckWinner()
+        {
+            if (UncompletedBlueGoalsLocations.Count == 0)
+                return TeamColor.Blue;
+            else if (UncompletedRedGoalsLocations.Count == 0)
+                return TeamColor.Red;
+            else
+                throw new InvalidOperationException();
         }
     }
 }
