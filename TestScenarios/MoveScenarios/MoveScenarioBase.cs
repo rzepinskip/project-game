8079419421cc81c29544dc43536175ff917dfa8e
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-03-30 09:54:15 +0200

    Add move scenario stub

---
+++ TestScenarios/MoveScenarios/MoveScenarioBase.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using Common.Interfaces;
+using Messaging.Requests;
+
+namespace TestScenarios.MoveScenarios
+{
+    public abstract class MoveScenarioBase : ScenarioBase
+    {
+        protected int PlayerId { get; set; } = 0;
+
+        public override IRequest InitialRequest { get; set; } = new MoveRequest(PlayerId, Direction.Down);
+    }
+}
---
+++ TestScenarios/ScenarioBase.cs
@@ -0,0 +1,35 @@
+﻿using Common.Interfaces;
+
+namespace TestScenarios
+{
+    public abstract class ScenarioBase
+    {
+        protected ScenarioBase(Game)
+        {
+            InitialPlayerBoard = initialPlayerBoard;
+            InitGameMasterBoard = initGameMasterBoard;
+            InitialRequest = initialRequest;
+            UpdatedGameMasterBoard = updatedGameMasterBoard;
+            Response = response;
+            UpdatedPlayerBoard = updatedPlayerBoard;
+        }
+
+        public abstract IPlayerBoard InitialPlayerBoard { get; set; }
+        public abstract IGameMasterBoard InitGameMasterBoard { get; set; }
+        public abstract IRequest InitialRequest { get; set; }
+
+        public abstract IGameMasterBoard UpdatedGameMasterBoard { get; set; } // Validate&Response assert
+        public abstract IResponse Response { get; set; } // Response assert, UpdatePlayer input
+        public abstract IPlayerBoard UpdatedPlayerBoard { get; set; } // UpdatePlayer output
+
+        protected IBoard LoadGameMasterBoard()
+        {
+
+        }
+
+        protected IBoard LoadPlayerBoard()
+        {
+
+        }
+    }
+}
