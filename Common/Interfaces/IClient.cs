f81e75b2b0e3ebfd699079ea05a8e7ed379dbf55
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-04-08 11:54:56 +0200

    Add IClient and IProxy interfaces with implementation
    pair programming: szymanskir

---
+++ Common/Interfaces/IClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common.Interfaces
+{
+    public interface IClient
+    {
+        void Send(string data);
+
+        void SetupClient(Action<string> messageHandler);
+    }
+}
---
+++ Common/Interfaces/IProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Interfaces
+{
+    public interface IProxy
+    {
+        //IClient Client { get; set; }
+
+        void Send(IMessage message);
+        void SubscribeProxy(Action<IMessage> messageHandler);
+
+    }
+}
