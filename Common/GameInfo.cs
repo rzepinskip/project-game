c675120aec27cded8771c982c12b8e69929d16ce
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-04-09 13:10:25 +0200

    Add GameInfo class
    pair programming: sowiks

---
+++ Common/GameInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Common
+{
+    public class GameInfo
+    {
+        public string GameName { get; set; }
+        public int BlueTeamPlayers { get; set; }
+        public int RedTeamPlayers { get; set; }
+
+        public GameInfo(string gameName, int blueTeamPlayers, int redTeamPlayers)
+        {
+            GameName = gameName;
+            BlueTeamPlayers = blueTeamPlayers;
+            RedTeamPlayers = redTeamPlayers;
+        }
+    }
+}
