b3070c760d3cb91c767468ec30678513322453e1
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-04-13 21:55:58 +0200

    Add CommunicationStateObject
    pair programming: sowiks

---
+++ Common/Communication/CommunicationStateObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Common.Communication
+{
+    public class CommunicationStateObject
+    {
+        public const int BufferSize = 1024;
+        public const char ETBByte = (char)23;
+        public byte[] Buffer { get; } =  new byte[BufferSize];
+        public StringBuilder Sb { get; } = new StringBuilder();
+        public Socket WorkSocket { get; set; }
+        public ManualResetEvent MessageProcessed { get; } = new ManualResetEvent(true);
+        public int SocketID { get; }
+
+        public CommunicationStateObject(Socket workSocket, int socketId = 0)
+        {
+            SocketID = socketId;
+            WorkSocket = workSocket;
+        }
+    }
+}
