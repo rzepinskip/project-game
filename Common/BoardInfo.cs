60964d3b24c9e1ff9b3e8215c104385d2bffd879
Mateusz Stelmaszek <themattpl007@gmail.com>
2018-04-21 10:51:31 +0200

    Add parameterless constructor for BoardInfo
    pair programming: szymanskir

--- Common/BoardInfo.cs
+++ Common/BoardInfo.cs
@@ -5,5 +5,16 @@
         public int Width { get; set; }
         public int TasksHeight { get; set; }
         public int GoalsHeight { get; set; }
+
+        public BoardInfo()
+        {
+        }
+
+        public BoardInfo(int width, int tasksHeight, int goalsHeight)
+        {
+            Width = width;
+            TasksHeight = tasksHeight;
+            GoalsHeight = goalsHeight;
+        }
     }
 }
