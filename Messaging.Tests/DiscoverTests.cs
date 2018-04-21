661ab9fda27ecb4f114b6921d36ab871a5c96191
Wojciech Trzesniowski <wojtek.trze@gmail.com>
2018-04-21 10:44:55 +0200

    create DiscoverScenarioBase

---
+++ Messaging.Tests/DiscoverTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Messaging.Serialization;
+using Org.XmlUnit.Builder;
+using Org.XmlUnit.Diff;
+using TestScenarios.DiscoverScenarios;
+using TestScenarios.MoveScenarios;
+using TestScenarios.MoveScenarios.MoveToGoalField;
+using TestScenarios.MoveScenarios.MoveToTasFieldWithoutPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldOccupiedByPlayerWhoDoesntCarryPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldWithPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldWithPieceOccupiedByPlayerWhoDoesntCarryPiece;
+using Xunit;
+
+namespace Messaging.Tests
+{
+    public class DiscoverTests
+    {
+        private const string DefaultNamespace = "https://se2.mini.pw.edu.pl/17-results/";
+
+        private ExtendedMessageXmlDeserializer _messageXmlSerializer =
+            new ExtendedMessageXmlDeserializer(DefaultNamespace);
+
+        private bool XmlsHasDiffrences(string expected, string actual)
+        {
+            var d = DiffBuilder.Compare(Input.FromString(expected)).WithTest(actual).Build();
+            return d.HasDifferences();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void MoveTestsRequest(DiscoverScenarioBase scenario)
+        {
+            var xml = _messageXmlSerializer.SerializeToXml(scenario.InitialRequest);
+
+            Assert.False(XmlsHasDiffrences(scenario.InitialRequestFileContent, xml));
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void MoveTestsResponse(DiscoverScenarioBase scenario)
+        {
+            var xml = _messageXmlSerializer.SerializeToXml(scenario.Response);
+
+            Assert.False(XmlsHasDiffrences(scenario.ResponseFileContent, xml));
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            //yield return new object[] { new DiscoverRegular() };
+            //yield return new object[] { new DiscoverTaskAreaEdge() };
+            //yield return new object[] { new DiscoverTaskAreaBoardEdge() };
+            //yield return new object[] { new DiscoverTaskAreaCorner() };
+            //yield return new object[] { new DiscoverGoalAreaCorner() };
+            //yield return new object[] { new DiscoverGoalAreaEdge() };
+            //yield return new object[] { new DiscoverBoardCorner() };
+            //yield return new object[] { new DiscoverPiece() };
+            //yield return new object[] { new DiscoverPlayer() };
+            //yield return new object[] { new DiscoverUpdate() };  }
+        }
+    }
+}
---
+++ Player.Tests/DiscoverTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using TestScenarios.DiscoverScenarios;
+using TestScenarios.MoveScenarios;
+using TestScenarios.MoveScenarios.MoveToGoalField;
+using TestScenarios.MoveScenarios.MoveToTasFieldWithoutPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldOccupiedByPlayerWhoDoesntCarryPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldWithPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldWithPieceOccupiedByPlayerWhoDoesntCarryPiece;
+using Xunit;
+
+namespace Player.Tests
+{
+    public class DiscoverTests
+    {
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void MoveTestsBoard(DiscoverScenarioBase scenario)
+        {
+            var player = new Player();
+            var playerInfo = scenario.InitialPlayerBoard.Players[scenario.PlayerId];
+            player.InitializePlayer(scenario.PlayerId, scenario.PlayerGuid, playerInfo.Team, playerInfo.Role,
+                scenario.InitialPlayerBoard, playerInfo.Location);
+
+            scenario.Response.Process(player);
+
+            Assert.Equal(scenario.UpdatedPlayerBoard, player.Board);
+        }
+
+
+        public static IEnumerable<object[]> GetData()
+        {
+            //yield return new object[] { new DiscoverRegular() };
+            //yield return new object[] { new DiscoverTaskAreaEdge() };
+            //yield return new object[] { new DiscoverTaskAreaBoardEdge() };
+            //yield return new object[] { new DiscoverTaskAreaCorner() };
+            //yield return new object[] { new DiscoverGoalAreaCorner() };
+            //yield return new object[] { new DiscoverGoalAreaEdge() };
+            //yield return new object[] { new DiscoverBoardCorner() };
+            //yield return new object[] { new DiscoverPiece() };
+            //yield return new object[] { new DiscoverPlayer() };
+            //yield return new object[] { new DiscoverUpdate() };
+        }
+    }
+}
