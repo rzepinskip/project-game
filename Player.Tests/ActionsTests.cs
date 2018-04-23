14a26862ded04ba7c522cdd0c949d3e78e0a799d
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-04-23 11:37:43 +0200

    Merge components' test classes into one

---
+++ Player.Tests/ActionsTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using TestScenarios;
+using Xunit;
+
+namespace Player.Tests
+{
+    public class ActionsTests
+    {
+        [Theory]
+        [ClassData (typeof(TestsDataset))]
+        public void TestActionBoardsUpdate(ScenarioBase scenario)
+        {
+            var player = new Player();
+            var playerInfo = scenario.InitialPlayerBoard.Players[scenario.PlayerId];
+            player.InitializePlayer(scenario.PlayerId, scenario.PlayerGuid, playerInfo.Team, playerInfo.Role,
+                scenario.InitialPlayerBoard, playerInfo.Location);
+
+            scenario.Response.Process(player);
+            
+            player.Board.Should().BeEquivalentTo(scenario.UpdatedPlayerBoard);
+            player.Board.Should().Be(scenario.UpdatedPlayerBoard);
+        }
+    }
+}
---
+++ TestScenarios/TestsDataset.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TestScenarios.DiscoverScenarios.DiscoverBoardCorner;
+using TestScenarios.DiscoverScenarios.DiscoverGoalAreaCorner;
+using TestScenarios.DiscoverScenarios.DiscoverGoalAreaEdge;
+using TestScenarios.DiscoverScenarios.DiscoverPiece;
+using TestScenarios.DiscoverScenarios.DiscoverPlayer;
+using TestScenarios.DiscoverScenarios.DiscoverRegular;
+using TestScenarios.DiscoverScenarios.DiscoverTaskAreaBoardEdge;
+using TestScenarios.DiscoverScenarios.DiscoverTaskAreaCorner;
+using TestScenarios.DiscoverScenarios.DiscoverTaskAreaEdge;
+using TestScenarios.DiscoverScenarios.DiscoverUpdate;
+using TestScenarios.MoveScenarios.MoveToGoalField;
+using TestScenarios.MoveScenarios.MoveToTasFieldWithoutPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldOccupiedByPlayerWhoDoesntCarryPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldWithPiece;
+using TestScenarios.MoveScenarios.MoveToTaskFieldWithPieceOccupiedByPlayerWhoDoesntCarryPiece;
+
+namespace TestScenarios
+{
+    public class TestsDataset : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _testScenarios = new List<object[]>();
+
+        public TestsDataset()
+        {
+            _testScenarios.AddRange(GetMoveTests());
+            _testScenarios.AddRange(GetDiscoverTests());
+        }
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _testScenarios.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private IEnumerable<object[]> GetMoveTests()
+        {
+            return new List<object[]>
+            {
+                new object[] {new MoveToGoalField()},
+                new object[] {new MoveToTaskFieldWithoutPiece()},
+                new object[] {new MoveToTaskFieldWithPiece()},
+                //new object[] { new MoveToTaskFieldOccupiedByPlayerWhoCarryPiece()},
+                new object[] {new MoveToTaskFieldOccupiedByPlayerWhoDoesntCarryPiece()},
+                //new object[] { new MoveToTaskFieldWithPieceOccupiedByPlayerWhoCarryPiece()},
+                new object[] {new MoveToTaskFieldWithPieceOccupiedByPlayerWhoDoesntCarryPiece()}
+            };
+        }
+
+        private IEnumerable<object[]> GetDiscoverTests()
+        {
+            return new List<object[]>
+            {
+                new object[] {new DiscoverRegular()},
+                new object[] {new DiscoverTaskAreaEdge()},
+                new object[] {new DiscoverTaskAreaBoardEdge()},
+                new object[] {new DiscoverTaskAreaCorner()},
+                new object[] {new DiscoverGoalAreaCorner()},
+                new object[] {new DiscoverGoalAreaEdge()},
+                new object[] {new DiscoverBoardCorner()},
+                new object[] {new DiscoverPiece()},
+                new object[] {new DiscoverPlayer()},
+                new object[] {new DiscoverUpdate()}
+            };
+        }
+    }
+}
