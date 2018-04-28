6851de3d82a232ae585698ca23bbfa127f244852
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-04-28 21:08:36 +0200

    Remove unused namespaces and constructors

--- CommunicationServer/CommunicationServerConverter.cs
+++ CommunicationServer/CommunicationServerConverter.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Common.Interfaces;
+﻿using Common.Interfaces;
 using Messaging;
-using Messaging.Serialization;
 
 namespace CommunicationServer
 {
     public class CommunicationServerConverter : MessageConverterBase
     {
-        public CommunicationServerConverter():base()
-        {
-        }
-
         public override IMessage ConvertStringToMessage(string message)
         {
             return MessageSerializer.Deserialize<Message>(message);
--- GameMaster/GameMasterConverter.cs
+++ GameMaster/GameMasterConverter.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Common.Interfaces;
+﻿using Common.Interfaces;
 using Messaging;
-using Messaging.Requests;
-using Messaging.Serialization;
 
 namespace GameMaster
 {
     public class GameMasterConverter : MessageConverterBase
     {
-        public GameMasterConverter():base()
-        {
-        }
         public override IMessage ConvertStringToMessage(string message)
         {
             return MessageSerializer.Deserialize<Message>(message);
