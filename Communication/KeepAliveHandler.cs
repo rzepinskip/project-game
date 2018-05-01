af87733f2bd5dedc99df71642aca592d4277546a
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-05-01 21:57:29 +0200

    Fix keepalive send and check interval

--- Communication/KeepAliveHandler.cs
+++ Communication/KeepAliveHandler.cs
@@ -6,25 +6,25 @@ namespace Communication
     public class KeepAliveHandler
     {
         private Timer _checkKeepAliveTimer;
-        protected readonly int _keepAliveTimeInterval;
-        protected readonly IKeepAliveGetter _keepAliveGetter;
+        protected readonly int KeepAliveTimeInterval;
+        protected readonly IKeepAliveGetter KeepAliveGetter;
         public KeepAliveHandler(int keepAliveTimeInterval, IKeepAliveGetter keepAliveGetter)
         {
-            _keepAliveGetter = keepAliveGetter;
-            _keepAliveTimeInterval = keepAliveTimeInterval / 2;
+            KeepAliveGetter = keepAliveGetter;
+            KeepAliveTimeInterval = keepAliveTimeInterval;
             _checkKeepAliveTimer = new Timer(KeepAliveCallback, null, 0,
-                _keepAliveTimeInterval);
+                KeepAliveTimeInterval/2);
         }
 
         private void KeepAliveCallback(object state)
         {
             var currentTime = DateTime.Now.Ticks;
-            foreach (var csStateObject in _keepAliveGetter.Get())
+            foreach (var csStateObject in KeepAliveGetter.Get())
             {
                 var elapsedTicks = currentTime - csStateObject.GetLastMessageReceivedTicks();
                 var elapsedSpan = new TimeSpan(elapsedTicks);
 
-                if (elapsedSpan.Milliseconds > _keepAliveTimeInterval)
+                if (elapsedSpan.Milliseconds > KeepAliveTimeInterval)
                     csStateObject.CloseSocket();
             }
         }
