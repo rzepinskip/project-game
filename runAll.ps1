40c63d186267420a5f2e60d3214c461e6dc59ef9
Ryszard Szymański <ryszard.szymanski@outlook.com>
2018-05-05 22:53:14 +0200

    Add runtime parameters to powershell script
    triple programming: rzepinskip, stelmaszekm

--- runAll.ps1
+++ runAll.ps1
@@ -1,22 +1,17 @@
-Copy-Item ".\GameMaster.App\bin\Debug\netcoreapp2.0\Resources" -Destination ".\Resources" -Recurse -ErrorAction SilentlyContinue
-
 $localname = hostname
 $address = Test-Connection $localname -count 1 | select Ipv6Address | ft -HideTableHeaders | Out-String
 $address = $address.Trim()
 $gameConfigFilePath = "ExampleConfig.xml"
+$portNumber = 11000
 
-echo ".\GameMaster.App\bin\Debug\netcoreapp2.0\GameMaster.App.dll --port 11000 --conf $gameConfigFilePath --address $address --game game"
-$address = echo $address
-echo $gameConfigFilePath
-
-Start-Process -FilePath "dotnet" -ArgumentList  ".\CommunicationServer.App\bin\Debug\netcoreapp2.0\CommunicationServer.App.dll --port 11000 --conf $gameConfigFilePath" 
+Start-Process -FilePath "dotnet" -ArgumentList  ".\CommunicationServer.App\bin\Debug\netcoreapp2.0\CommunicationServer.App.dll --port $portNumber --conf $gameConfigFilePath" 
 Start-Sleep -s 4
-Start-Process -FilePath "dotnet" -ArgumentList  ".\GameMaster.App\bin\Debug\netcoreapp2.0\GameMaster.App.dll --port 11000 --conf $gameConfigFilePath --address $address --game game"
+Start-Process -FilePath "dotnet" -ArgumentList  ".\GameMaster.App\bin\Debug\netcoreapp2.0\GameMaster.App.dll --port $portNumber --conf $gameConfigFilePath --address $address --game game"
 Start-Sleep -s 2
 
 
 For ($i=0; $i -le 1; $i++) 
 {
-    Start-Process -FilePath "dotnet" -ArgumentList  ".\Player.App\bin\Debug\netcoreapp2.0\Player.App.dll --port 11000 --conf $gameConfigFilePath --address $address --game game --team blue --role leader"
+    Start-Process -FilePath "dotnet" -ArgumentList  ".\Player.App\bin\Debug\netcoreapp2.0\Player.App.dll --port $portNumber --conf $gameConfigFilePath --address $address --game game --team blue --role leader"
     Start-Sleep -s 1
 }
