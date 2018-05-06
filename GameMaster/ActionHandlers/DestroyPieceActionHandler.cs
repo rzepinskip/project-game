2337d7b8edb43b57c34be649392bb484f3b1e7c3
Ryszard Szymański <ryszard.szymanski@outlook.com>
2018-05-06 11:15:31 +0200

    Add DestroyPieceActionHandler class

---
+++ GameMaster/ActionHandlers/DestroyPieceActionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common;
+using Common.BoardObjects;
+
+namespace GameMaster.ActionHandlers
+{
+    public class DestroyPieceActionHandler : ActionHandler
+    {
+        public DestroyPieceActionHandler(int playerId, GameMasterBoard board) : base(playerId, board)
+        {
+            PlayerId = playerId;
+            Board = board;
+        }
+
+        protected override bool Validate()
+        {
+            var playerInfo = Board.Players[PlayerId];
+            return false;
+        }
+
+        public override DataFieldSet Respond()
+        {
+            if(!Validate())
+                return DataFieldSet.Create(PlayerId, new Piece[0]);
+
+            var player = Board.Players[PlayerId];
+            var playerPiece = player.Piece;
+
+            player.Piece = null;
+            playerPiece.Type = PieceType.Destroyed;
+
+            return DataFieldSet.Create(PlayerId, new [] { playerPiece });
+        }
+    }
+}
