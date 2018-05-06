51f04e06b3005726d68c5333ebcffb4c3ce9fcea
Ryszard Szymański <ryszard.szymanski@outlook.com>
2018-05-06 11:01:35 +0200

    Add DestroyPieceRequest class

---
+++ Messaging/Requests/DestroyPieceRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+using Common;
+using Common.ActionInfo;
+
+namespace Messaging.Requests
+{
+    [XmlType(XmlRootName)]
+    public class DestroyPieceRequest : Request
+    {
+        public const string XmlRootName = "DestroyPiece";
+
+        protected DestroyPieceRequest() { }
+
+        public DestroyPieceRequest(Guid playerGuid, int gameId) : base(playerGuid, gameId)
+        {
+        }
+
+        public override ActionInfo GetActionInfo()
+        {
+            return new DestroyActionInfo(PlayerGuid);
+        }
+
+        public override string ToLog()
+        {
+            return String.Join(',', ActionType.Destroy, base.ToLog());
+        }
+    }
+}
