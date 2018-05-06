5f1e2bc9fb8f5faa38e6f01961826e1977300d5c
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-05-06 12:28:06 +0200

    Move Game Simulation default port variable

---
+++ GameSimulation/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSimulation
+{
+    public static class Constants
+    {
+        public const int DefaultPortNumber = 11000;
+    }
+}
