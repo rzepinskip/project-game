d0be2db5299c4719e57f76c79b24504e8c9fdd9e
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-05-10 22:07:19 +0200

    Add client disconnected messages

---
+++ Messaging/ErrorsMessages/GameMasterDisconnected.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Serialization;
+using Common.Interfaces;
+
+namespace Messaging.ErrorsMessages
+{
+    [XmlType(XmlRootName)]
+    public class GameMasterDisconnected : Message
+    {
+        public const string XmlRootName = "GameMasterDisconnected";
+
+        protected GameMasterDisconnected()
+        {
+        }
+
+        public GameMasterDisconnected(int gameId)
+        {
+            GameId = gameId;
+        }
+
+        [XmlAttribute("gameId")] public int GameId { get; set; }
+
+        public override IMessage Process(IGameMaster gameMaster)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Process(IPlayer player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Process(ICommunicationServer cs, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
