b960030ffc0d048b924d6b2e26bab17046d6ea50
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-05-18 14:26:56 +0200

    Add example game strategy states and transitions

---
+++ StateCoordinator/States/GameStrategyStates/DestroyPieceStrategyState.cs
@@ -0,0 +1,19 @@
+﻿using PlayerStateCoordinator.Info;
+using PlayerStateCoordinator.Transitions;
+using PlayerStateCoordinator.Transitions.GameStrategyTransitions;
+
+namespace PlayerStateCoordinator.States.GameStrategyStates
+{
+    public class DestroyPieceStrategyState : GameStrategyState
+    {
+        public DestroyPieceStrategyState(GameStrategyInfo gameStrategyInfo) : base(
+            StateTransitionType.Triggered,
+            new Transition[]
+            {
+                new HasNoPieceTransition(gameStrategyInfo)
+            },
+            gameStrategyInfo)
+        {
+        }
+    }
+}
---
+++ StateCoordinator/Transitions/GameStrategyTransitions/HasNoPieceTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Common.Interfaces;
+using Messaging.Requests;
+using PlayerStateCoordinator.Info;
+using PlayerStateCoordinator.States;
+
+namespace PlayerStateCoordinator.Transitions.GameStrategyTransitions
+{
+    public class HasNoPieceTransition : GameStrategyTransition
+    {
+        public HasNoPieceTransition(GameStrategyInfo gameStrategyInfo) : base(gameStrategyInfo)
+        {
+        }
+
+        public override State NextState => new DiscoverStrategyState(GameStrategyInfo);
+
+        public override IEnumerable<IMessage> Message = new List<IMessage> {new DiscoverRequest(GameStrategyInfo.PlayerGuid, GameStrategyInfo.GameId)};
+
+        public override bool IsPossible()
+        {
+            var playerInfo = GameStrategyInfo.Board.Players[GameStrategyInfo.PlayerId];
+            return playerInfo.Piece == null;
+        }
+    }
+}
