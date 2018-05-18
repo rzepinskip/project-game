06e433e3de0bbdce654bccd14ce17ed384da143a
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-05-18 13:02:40 +0200

    Add PlayerStateCoordinator project

---
+++ StateCoordinator/Info/BaseInfo.cs
@@ -0,0 +1,15 @@
+﻿using Common.Interfaces;
+using PlayerStateCoordinator.States;
+
+namespace PlayerStateCoordinator.Info
+{
+    public abstract class BaseInfo : ILoggable
+    {
+        public State PlayerStrategyBeginningState { get; set; }
+
+        public string ToLog()
+        {
+            return ToString();
+        }
+    }
+}
---
+++ StateCoordinator/Info/GameInitializationInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Common;
+using PlayerStateCoordinator.States;
+using PlayerStateCoordinator.States.InitializationStates;
+
+namespace PlayerStateCoordinator.Info
+{
+    public class GameInitializationInfo : BaseInfo
+    {
+        public readonly string GameName;
+
+        public GameInitializationInfo(string gameName, TeamColor color, PlayerType role)
+        {
+            GameName = gameName;
+            Color = color;
+            Role = role;
+        }
+
+        public State PlayerGameInitializationBeginningState => new GetGamesState(this);
+
+        public bool IsGameRunning { get; set; }
+        public IEnumerable<GameInfo> GamesInfo { get; set; }
+        public TeamColor Color { get; }
+        public PlayerType Role { get; }
+        public bool JoiningSuccessful { get; set; }
+    }
+}
