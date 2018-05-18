4a5380d02ad1804e3482e298947ac53973b68a62
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2018-05-18 15:57:44 +0200

    Add basic strategy objects

--- StateCoordinator/States/GameStrategyState.cs
+++ StateCoordinator/States/GameStrategyState.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Common.Interfaces;
+using Messaging;
+using Messaging.KnowledgeExchangeMessages;
 using PlayerStateCoordinator.Info;
 using PlayerStateCoordinator.Transitions;
 
@@ -8,21 +10,43 @@ namespace PlayerStateCoordinator.States
 {
     public class GameStrategyState : State
     {
-        private readonly GameStrategyInfo _gameStrategyInfo;
+        protected readonly GameStrategyInfo GameStrategyInfo;
 
-        public GameStrategyState(StateTransitionType transitionType, IEnumerable<Transition> transitions, GameStrategyInfo gameStrategyInfo) : base(transitionType, transitions, gameStrategyInfo)
+        public GameStrategyState(StateTransitionType transitionType,
+            GameStrategyInfo gameStrategyInfo) : base(transitionType, gameStrategyInfo)
         {
-            _gameStrategyInfo = gameStrategyInfo;
+            GameStrategyInfo = gameStrategyInfo;
         }
 
-        protected override Transition HandleRequestMessage(IRequestMessage requestMessage)
+        protected override Transition HandleResponseMessage(IResponseMessage responseMessage)
         {
-           throw new NotImplementedException();
+            if (responseMessage is KnowledgeExchangeRequestMessage knowledgeExchangeRequest)
+            {
+                var initiatorId = knowledgeExchangeRequest.SenderPlayerId;
+                Console.WriteLine($"Player #{initiatorId} requested communication");
+                IMessage knowledgeExchangeResponse =
+                    new RejectKnowledgeExchangeMessage(GameStrategyInfo.PlayerId, initiatorId);
+
+                //if (GameStrategyInfo.Board.Players[initiatorId].Team  == GameStrategyInfo.Team)
+                //    knowledgeExchangeResponse =
+                //        DataMessage.FromBoardData(GameStrategyInfo.Board.ToBoardData(GameStrategyInfo.PlayerId, initiatorId), false, GameStrategyInfo.PlayerGuid);
+
+                return new LoopbackTransition(this, new List<IMessage> {knowledgeExchangeResponse});
+            }
+
+            if (responseMessage is DataMessage dataMessage && dataMessage.GoalFields.Length > 1)
+            {
+                Console.WriteLine($"Got some data, doing nothing");
+
+                return new LoopbackTransition(this, new List<IMessage>());
+            }
+
+            return base.HandleResponseMessage(responseMessage);
         }
 
         protected override Transition HandleErrorMessage(IErrorMessage errorMessage)
         {
-            return new ErrorTransition(_gameStrategyInfo.PlayerGameStrategyBeginningState);
+            return new ErrorTransition(GameStrategyInfo.PlayerGameStrategyBeginningState);
         }
     }
 }
