569666e6db0356a56fae0b43162d638530459fec
sowiks2711 <sowiks@student.mini.pw.edu.pl>
2018-05-20 14:49:20 +0200

    Extract base strategy class from player strategy

---
+++ Player/NormalPlayerStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Common.BoardObjects;
+using PlayerStateCoordinator.Info;
+using PlayerStateCoordinator.States;
+using PlayerStateCoordinator.States.GameStrategyStates;
+
+namespace Player
+{
+    public class NormalPlayerStrategy : Strategy
+    {
+
+        public NormalPlayerStrategy(PlayerBase player, BoardBase board, Guid playerGuid, int gameId) :base(player, board, playerGuid, gameId)
+        {
+            BeginningState = new InitStrategyState(GameStrategyInfo);
+        }
+
+        public override State GetBeginningState()
+        {
+            return BeginningState;
+        }
+    }
+}
--- Player/Strategy.cs
+++ Player/Strategy.cs
@@ -4,16 +4,17 @@ using Common;
 using Common.BoardObjects;
 using PlayerStateCoordinator.Info;
 using PlayerStateCoordinator.States;
-using PlayerStateCoordinator.States.GameStrategyStates;
 
 namespace Player
 {
-    public class PlayerStrategy
+    public abstract class Strategy
     {
-        private readonly GameStrategyState _beginningState;
+        protected NormalPlayerStrategyState BeginningState;
+        protected GameStrategyInfo GameStrategyInfo;
 
-        public PlayerStrategy(PlayerBase player, BoardBase board, Guid playerGuid, int gameId)
+        protected Strategy(PlayerBase player, BoardBase board, Guid playerGuid, int gameId)
         {
+
             var teamCoefficient = player.Team == TeamColor.Blue ? 0 : 1;
             var offset = teamCoefficient * (board.TaskAreaSize + board.GoalAreaSize);
             var undiscoveredGoalFields = new List<GoalField>();
@@ -24,14 +25,10 @@ namespace Player
 
             undiscoveredGoalFields.Shuffle();
 
-            var gameStrategyInfo = new GameStrategyInfo(board, player.Id, playerGuid, gameId, player.Team,
+            GameStrategyInfo = new GameStrategyInfo(board, player.Id, playerGuid, gameId, player.Team,
                 undiscoveredGoalFields, new Location(board.Width / 2, board.GoalAreaSize + 1));
-            _beginningState = new InitStrategyState(gameStrategyInfo);
         }
 
-        public State GetBeginningState()
-        {
-            return _beginningState;
-        }
+        public abstract State GetBeginningState();
     }
 }
