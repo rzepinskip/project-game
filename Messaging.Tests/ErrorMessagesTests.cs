50d071baa9c9eb18b53a1a7493578376a0fda917
Ryszard Szymański <ryszard.szymanski@outlook.com>
2018-05-20 12:23:39 +0200

    Add error messages tests

---
+++ Messaging.Tests/ErrorMessagesTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using FluentAssertions;
+using Messaging.Serialization;
+using Xunit;
+
+namespace Messaging.Tests
+{
+    public class ErrorMessagesTests
+    {
+        private const string DefaultNamespace = "https://se2.mini.pw.edu.pl/17-results/";
+
+        protected ExtendedMessageXmlDeserializer MessageXmlSerializer =
+            new ExtendedMessageXmlDeserializer(DefaultNamespace);
+
+        public static IEnumerable<object[]> ErrorMessagesTypes =>
+            new List<object[]>
+            {
+                new object[] {"GameMasterDisconnectedMessage"},
+                new object[] {"PlayerDisconnectedMessage"},
+            };
+
+        [Theory]
+        [MemberData(nameof(ErrorMessagesTypes))]
+        public void TestErrorMessages(string filename)
+        {
+            var filePath = $"Resources/ErrorMessages/{filename}.xml";
+            var expected = File.ReadAllText(filePath);
+
+            var message = MessageXmlSerializer.Deserialize(expected);
+            var result = message.SerializeToXml();
+
+            XDocument.Parse(result).Should().BeEquivalentTo(XDocument.Parse(expected));
+        }
+    }
+}
